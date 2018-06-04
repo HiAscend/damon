e72e519692e6b1869f680faa627fc98a47eee9df
HiAscend <poseidon1237@163.com>
2018-06-04 16:06:18 +0800

    代码提交

--- src/test/java/cn/edu/zua/damon/dao/PictureDaoTest.java
+++ src/test/java/cn/edu/zua/damon/dao/PictureDaoTest.java
@@ -1,9 +1,14 @@
 package cn.edu.zua.damon.dao;
 
+import cn.edu.zua.damon.entity.Picture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
+
+import java.util.Date;
 
 /**
  * PictureDaoTest
@@ -13,6 +18,7 @@ import org.testng.annotations.Test;
  */
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class PictureDaoTest extends AbstractTestNGSpringContextTests {
+    private static final Logger LOG = LoggerFactory.getLogger(PictureDaoTest.class);
     private PictureDao pictureDao;
 
     public PictureDaoTest() {
@@ -25,6 +31,21 @@ public class PictureDaoTest extends AbstractTestNGSpringContextTests {
 
     @Test
     public void testFindPictureById() {
+        Picture p1 = pictureDao.findPictureById(1L);
+        LOG.debug("picture:{}", p1);
+    }
 
+    @Test
+    public void test() {
+        Picture picture = new Picture();
+        picture.setId(2L);
+        picture.setPath("/tmp/zs.png");
+        picture.setType(1);
+        picture.setTargetUrl("https://img-blog.csdn.net/20180523141459552?watermark/2/text/aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3VfYXNjZW5k/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70");
+        picture.setGrade(1);
+        Date now = new Date();
+        picture.setGmtCreate(now);
+        picture.setGmtModified(now);
+        LOG.debug("picture:{}", picture);
     }
 }
